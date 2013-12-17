ea66224edd9bc3cef723e15bc84fbb6fdf4cf20d
Brandon Dewitt <brandonsdewitt@gmail.com>
2013-12-17 10:55:50 -0700

    add vertica5/7 support in adapter requires

---
+++ lib/active_record/connection_adapters/vertica5_adapter.rb
@@ -0,0 +1,2 @@
+# Looks like Vertica5 shares with Vertica6
+require 'active_record/connection_adapters/vertica6_adapter'
---
+++ lib/active_record/connection_adapters/vertica7_adapter.rb
@@ -0,0 +1,4 @@
+require 'java'
+require 'activerecord-jdbcvertica-adapter/jars/vertica-jdbc-7.0.0-0.jar'
+
+require 'arjdbc/vertica'
