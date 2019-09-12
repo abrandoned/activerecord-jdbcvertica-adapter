1878f5d04531bf72058edf21c841084da613e4f8
Derek Adams <derek.adams@mx.com>
2019-09-12 17:03:09 -0600

    Compare type.to_sym

--- lib/arjdbc/vertica/adapter.rb
+++ lib/arjdbc/vertica/adapter.rb
@@ -279,7 +279,7 @@ module ::ArJdbc
     end
 
     def type_to_sql(type, limit = nil, precision = nil, scale = nil) #:nodoc:
-      super unless type == :integer
+      super unless type.to_sym == :integer
 
       if native = native_database_types[type.to_sym]
         (native.is_a?(Hash) ? native[:name] : native).dup
